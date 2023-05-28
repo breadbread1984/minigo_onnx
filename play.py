e240c9c352317329b0a2b2b5e20b7ab3f1e742c1
yi.xie <yi.xie@enflame-tech.com>
2023-05-28 12:53:33 +0800

    update

--- play.py
+++ play.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 
+from absl import app, flags;
+from strategies import CGOSPlayer, MCTSPlayer;
 
+FLAGS = flags.FLAGS
+
+def add_options():
+  flags.DEFINE_enum('mode', enum_values = {'cgos', 'kgs'}, default = 'cgos', help = 'game mode');
+  flags.DEFINE_enum('device', enum_values = {'cpu', 'gpu'}, default = 'cpu', help = 'device to use');
+
+def main(unused_argv):
+  player = CGOSPlayer(network)
+
+if __name__ == "__main__":
+  add_options();
+  app.run(main);
